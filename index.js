5a9fc945b84d226cdd5d8988777be6aa066c21f0
TS <77306441+TS2846@users.noreply.github.com>
2024-09-22 19:07:20 -0700

    chat features

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.listen(port, ()=> console.log(`Dolphin app listening on port ${port}!`))
+app.listen(port, ()=> console.log(`Message app listening on port ${port}!`))
 
-
+app.use(express.static(__dirname));
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '\\index.html');
