cf9bbdecde9c872fcaf9bc92f9fc902e9fa67723
Deepta Adhikary <deeptoadhikary@gmail.com>
2024-10-13 09:22:24 -0400

    minor refactors

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,49 +1,85 @@
-// webapp behavior
-const joinRoomButton = document.getElementById("room-button")
-const messageInput = document.getElementById("message-input")
-const messageContainer = document.getElementById("message-container")
-const roomInput = document.getElementById("room-input")
-const form = document.getElementById("form")
-
 const socket = io();
-let currentRoom = "";
 
+window.onload = main;
 
-// listener calls displaymessage
-form.addEventListener("submit", e => {
-    e.preventDefault()
-    const message = messageInput.value
-    const roomId = roomInput.value
-    
-    if (message) {
-        displayMessage(message, "sent-message")
-        socket.emit('message', message, roomId);
-        messageInput.value = ""
+function main() {
+    const messageContainer = document.getElementById('message-container');
+    const messageInput = document.getElementById('message-input');
+    const roomInput = document.getElementById('room-input');
+    const joinRoomButton = document.getElementById('room-button');
+    const form = document.getElementById('form');
+
+    let roomId = '';
+
+    function displayMessage(message, message_class) {
+        const div = document.createElement('div');
+        div.classList.add(message_class);
+        div.textContent = message;
+        messageContainer.append(div);
     }
-})
 
-joinRoomButton.addEventListener("click", e => {
-    e.preventDefault()
-    const roomId = roomInput.value
-    console.log("Join room " + roomId)
+    function joinRoom(newRoomId) {
+        roomId = newRoomId;
 
-    if (roomId) {
-        socket.emit('join-room', roomId);
+        messageContainer.innerHTML = '';
+        displayMessage('Connected with id: ' + socket.id, 'notification');
+        displayMessage('Joined Chat Room: ' + roomId, 'notification');
+        roomInput.value = roomId;
     }
-})
 
-function displayMessage(message, message_class) {
-    const div = document.createElement("div");
-    div.classList.add(message_class);
-    div.textContent = message;
-    messageContainer.append(div);
+    socket.on('connect', () => {
+        displayMessage('Connected with id: ' + socket.id, 'notification');
+    });
+
+    socket.on('message:recieve', (message) => {
+        displayMessage(message, 'recieved-message');
+    });
+
+    socket.on('room:new_member', (userId) => {
+        displayMessage(userId + ' has joined the chatroom', 'notification');
+    });
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const message = messageInput.value;
+
+        if (!socket.connected) {
+            displayMessage(
+                'Not connected to server yet. Please wait...',
+                'notification',
+            );
+            return;
+        }
+
+        if (!roomId) {
+            displayMessage(
+                'You are not connected to a chatroom. Please join a chatroom.',
+                'error',
+            );
+            return;
+        }
+
+        if (message) {
+            displayMessage(message, 'sent-message');
+            socket.emit('message:sent', message, roomId);
+            messageInput.value = '';
+        }
+    });
+
+    joinRoomButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        const newRoomId = roomInput.value;
+        if (newRoomId) {
+            socket.emit('room:join', newRoomId, (response) => {
+                if (response.status === 'ok') {
+                    joinRoom(newRoomId);
+                } else {
+                    displayMessage(
+                        'Failed to connect to room ' + newRoomId,
+                        'notification',
+                    );
+                }
+            });
+        }
+    });
 }
-
-socket.on('message-recieve', (message) => {
-    displayMessage(message, "recieved-message");
-});
-
-socket.on('join-room', (roomId) => {
-    currentRoom = roomId;
-    displayMessage("Joined Chat Room: " + roomId, "notification");
-})
