3b37ef33f16936d872cae063685eff2abe713074
TS <77306441+TS2846@users.noreply.github.com>
2024-11-21 14:09:48 -0800

    FEAT: added friend list

--- client/src/components/FriendHeader/FriendHeader.tsx
+++ client/src/components/FriendHeader/FriendHeader.tsx
@@ -1,5 +1,4 @@
 import {Users} from 'lucide-react';
-import {Button} from '../ui/button';
 
 const FriendHeader = () => {
     return (
--- client/src/components/FriendWindow/FriendWindow.tsx
+++ client/src/components/FriendWindow/FriendWindow.tsx
@@ -1,12 +1,36 @@
 import {ScrollArea, ScrollBar} from '@/components/ui/scroll-area.tsx';
+import useContacts from '@/hooks/useContacts';
+import {PiChatCircleDotsFill} from 'react-icons/pi';
+import MiniProfile from '../MiniProfile';
+import {useCurrentRoom} from '@/contexts/RoomContext';
+import {usePageStatus} from '@/contexts/PageStatusContext';
 
 export default function FriendWindow() {
+    const {data} = useContacts();
+    const setCurrentRoomID = useCurrentRoom()[1];
+    const setPageStatus = usePageStatus()[1];
     return (
         <ScrollArea className="h-full overflow-y-auto bg-primary-foreground">
-            <div className="mx-5 h-96 my-2">
-                THIS IS THE FUCKING FRIEND WINDOW YOU
-                RETARD:1993_0_0::1993_1_0::1993_2_0::1993_3_0::1993_4_0::1993_5_0::1993_6_0:
-                :1993_0_1::1993_1_1::1993_2_1::1993_3_1::1993_4_1::1993_5_1::1993_6_1:
+            <div className="flex flex-col gap-3 mt-2">
+                {data?.map(friend => (
+                    <div
+                        className="flex flex-row p-3 rounded-md hover:bg-accent cursor-pointer justify-between"
+                        onClick={() => {
+                            setCurrentRoomID(friend.room_id);
+                            setPageStatus('room');
+                        }}
+                    >
+                        <div className="flex flex-row items-center justify-center gap-3">
+                            <MiniProfile
+                                title={friend.username}
+                                status="online"
+                            />
+                        </div>
+                        <div className="p-2 rounded-full bg-yellow-500 hover:bg-yellow-600">
+                            <PiChatCircleDotsFill size={25} />
+                        </div>
+                    </div>
+                ))}
             </div>
             <ScrollBar />
         </ScrollArea>
